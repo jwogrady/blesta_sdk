33f63db68ca31aec28436590b8ecf4483561b1c9
John W. O'Grady <john@status26.com>
2025-01-17 20:26:24 -0600

    add --last-request flag

--- blesta_cli.py
+++ blesta_cli.py
@@ -1,8 +1,8 @@
 import argparse
 import os
+import json
 from dotenv import load_dotenv
 from api.blesta_api import BlestaApi
-import json
 
 # Load environment variables from .env file
 load_dotenv()
@@ -13,6 +13,7 @@ def main():
     parser.add_argument('--method', required=True, help='Blesta API method (e.g., getList)')
     parser.add_argument('--action', choices=['GET', 'POST', 'PUT', 'DELETE'], default='GET', help='HTTP action')
     parser.add_argument('--params', nargs='*', help='Optional key=value pairs (e.g., id=1 status=active)')
+    parser.add_argument('--last-request', action='store_true', help='Show the last API request made')
 
     args = parser.parse_args()
 
@@ -36,9 +37,19 @@ def main():
 
     # Print the response
     if response.response_code == 200:
-        print("Success:", json.dumps(response.response, indent=4))
+        formatted_response = json.dumps(response.response, indent=4)
+        print(formatted_response)
     else:
-        print("Error:", response.errors)
+        print("Error:", response.errors())
+
+    # Show last request details if --last-request is called
+    if args.last_request:
+        last_request = api.get_last_request()
+        if last_request:
+            print("\nLast Request URL:", last_request['url'])
+            print("Last Request Parameters:", last_request['args'])
+        else:
+            print("No previous API request made.")
 
 if __name__ == "__main__":
     main()
